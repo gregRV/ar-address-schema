23de1d92389f5232caf0b06961a25b75535278c4
Gregory Varias <gregvarias@gmail.com>
2013-06-19 19:56:56 -0700

    completed solo #10

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,21 @@
 require_relative '../../db/config'
 
 # implement your Contact model here
+class Contact
+	validates :email, :format => 
+		{ :with => /\w+@\w+\.\w{2,}/ }
+
+	validates :phone, :format => 
+		{ :with => /\d{3}\D\d{3}\D\d{4}/ }
+
+	validates :company, :presence => true
+
+
+	def name
+		first_name + " " + last_name
+	end
+
+	def area_code
+		phone.slice(0..2)
+	end
+end
